0046e7ab21c38d8ae1312a8dcf549ef299d8503a
Paulo Rogério Moreira Rocha <pr.moreirarocha@gmail.com>
2020-05-11 07:51:24 -0300

    Teste for

---
+++ Conceito_do_while/Conceito_do_while/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conceito_do_while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+
+            int c = 0;
+            do
+            {
+                c = c + 1;
+                label1.Text = Convert.ToString(c);
+                label1.Refresh();
+            } while (c < 10000);
+        }
+    }
+}
